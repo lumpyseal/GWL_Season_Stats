4dd031cf3d9ad613f9417989ff85316dc83b3ccf
lumpyseal <lumpyseal@gmail.com>
2022-07-04 18:58:45 -0400

    Account for player disconnects and players not selecting heroes until after the match begins.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import numbers
 from pathlib import Path
 
 import pandas as pd
@@ -7,26 +8,24 @@ import utils
 from enums import Arg, LogCol, StatsCol
 
 pd.set_option('display.max_rows', 100000)
-DEFAULT_LOGS_DIR = Path.cwd() / 'game_logs'''
-
-# May God forgive me for my use of nested for loops
+DEFAULT_LOGS_DIR = Path.cwd() / 'game_logs'
 
 
-def parse_args():
-    arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument(
-        '-d',
-        '--logs-dir',
-        default=str(DEFAULT_LOGS_DIR),
-        dest=Arg.LOGS_DIR.value
-    )
-    arg_parser.add_argument(
-        '-o',
-        '--output-file',
-        default=str(Path.cwd() / 'season_stats.csv'),
-        dest=Arg.OUTPUT.value
-    )
-    return arg_parser.parse_args()
+# def parse_args():
+#     arg_parser = argparse.ArgumentParser()
+#     arg_parser.add_argument(
+#         '-d',
+#         '--logs-dir',
+#         default=str(DEFAULT_LOGS_DIR),
+#         dest=Arg.LOGS_DIR.value
+#     )
+#     arg_parser.add_argument(
+#         '-o',
+#         '--output-file',
+#         default=str(Path.cwd() / 'season_stats.csv'),
+#         dest=Arg.OUTPUT.value
+#     )
+#     return arg_parser.parse_args()
 
 
 def split_match_log_by_map(lines: [str]) -> []:
@@ -38,7 +37,7 @@ def split_match_log_by_map(lines: [str]) -> []:
     for line_num in lines_iter:
         if lines[line_num][0] == map:
             map_logs.append(lines[map_start_idx:line_num])
-            map_start_idx = next(lines_iter, None)  + 1  # Skip next line (players) as well
+            map_start_idx = next(lines_iter, None) + 1  # Skip next line (players) as well
 
     # end of last map
     map_logs.append(lines[map_start_idx:len(lines)])
@@ -66,7 +65,8 @@ def get_map_logs(lines: [str]) -> [pd.DataFrame]:
 
 
 def load_match_log(log_path: Path) -> [pd.DataFrame]:
-    with open(log_path, 'r') as log_file:
+    # UTF-8 cuz GrigmaMale is a ho, respectfully
+    with open(log_path, 'r', encoding='utf8') as log_file:
         lines = log_file.readlines()
     lines = utils.format_lines(lines)
 
@@ -105,7 +105,60 @@ def update_player_stats(player_stats, stats_diff, curr_hero) -> None:
         player_stats[curr_hero] = stats_diff
 
 
-def calculate_player_stats(player_log: [pd.DataFrame]) -> {str: {}}:
+def stats_all_zero(stat_row: dict):
+    """
+    Check if players stats (dmg, healing, elims, etc.) are all 0.
+
+    This does NOT include seconds played on purpose.
+    """
+
+    # Columns that get reset when a person disconnects
+    columns = set(col for col in LogCol) - set([LogCol.SECONDS, LogCol.PLAYER, LogCol.HERO])
+
+    return sum(stat_row[col] for col in columns) == 0
+
+
+def hero_selected(log_row: dict) -> bool:
+    return bool(log_row[LogCol.HERO])
+
+
+def start_df_at_hero_select(df: pd.DataFrame) -> pd.DataFrame:
+    heroes = df[LogCol.HERO]
+    for index, hero in heroes.iteritems():
+        if hero:
+            hero_selected_idx = index
+            break
+
+    df = df.drop(index=[n for n in range(0, hero_selected_idx)]).reset_index(drop=True)
+    hero_select_row = df.loc[hero_selected_idx].to_dict()
+
+    return hero_select_row, df
+
+
+def skip_inaction(log: pd.DataFrame):
+    """
+    Returns the first row (start of current hero) and skips to where the
+    action starts int he dataframe (player starts damaging, healing, etc.)
+    """
+
+    df = log.reset_index(drop=True)
+    first_row = df.head(1).to_dict('records')[0]
+    if not hero_selected(first_row):
+        first_row, df = start_df_at_hero_select(df)
+
+    for row in df.itertuples():
+        row = row._asdict()
+        if not stats_all_zero(row):
+            action_start_idx = row['Index']
+            break  # We've found where the action has started
+
+    df = df.drop(index=[n for n in range(0, action_start_idx)])
+    df = df.reset_index(drop=True)  # Reset index for future indexing operations
+
+    return first_row, df
+
+
+def calculate_player_stats(log: [pd.DataFrame]) -> {str: {}}:
     """
     Get stats for a single player from a that player's match log.
 
@@ -116,19 +169,38 @@ def calculate_player_stats(player_log: [pd.DataFrame]) -> {str: {}}:
     """
 
     player_stats = {}
-    curr_hero_start = player_log.head(1).to_dict('records')[0]  # First row as dict
+    # player_log = log.copy()
+    # curr_hero_start = player_log.head(1).to_dict('records')[0]  # First row as dict
+    curr_hero_start, player_log = skip_inaction(log)
     curr_hero = curr_hero_start[LogCol.HERO]  # Starting hero
-    for row in player_log.itertuples(index=False):
+    prev_row = None  # Track prev row in case of player disconnect
+    for row in player_log.itertuples():
+        row = row._asdict()
+        if stats_all_zero(row):  # Player disconnected
+            if prev_row:
+                # Calculate the stats accumulated on the hero prior to disconnect
+                stats_diff = calculate_stat_diff(curr_hero_start, prev_row)
+                update_player_stats(player_stats, stats_diff, curr_hero)
+
+            post_dc_log = player_log.drop(index=[n for n in range(0, row['Index'])])
+            post_dc_player_stats = calculate_player_stats(post_dc_log)
+            for hero in post_dc_player_stats:
+                stats_diff = post_dc_player_stats[hero]
+                update_player_stats(player_stats, stats_diff, hero)
+
+            # rest of stats already processed through recursive call
+            return player_stats
+
         # Hero swap
-        if row.HERO != curr_hero_start[LogCol.HERO]:
-            row = row._asdict()
-            # Calculate the stats accumulated on the hero they just swapped off
+        if row[LogCol.HERO] != curr_hero_start[LogCol.HERO]:
+            # Calculate the stats accumulated on hero they swapped off of
             stats_diff = calculate_stat_diff(curr_hero_start, row)
             update_player_stats(player_stats, stats_diff, curr_hero)
-
             # Update our trackers
-            curr_hero = row[LogCol.HERO]
             curr_hero_start = row
+            curr_hero = curr_hero_start[LogCol.HERO]
+
+        prev_row = row
 
     # Game ended
     last_row = player_log.tail(1).to_dict('records')[0]  # Last row as dict
@@ -186,23 +258,21 @@ def reduce_to_df(season_stats: dict) -> pd.DataFrame:
     stats_records = []
     for player in season_stats:
         for hero in season_stats[player]:
-            # '|' is the dict merge operator in python 3.9+
-            record = {StatsCol.PLAYER: player, StatsCol.HERO: hero} | season_stats[player][hero]
+            record = {StatsCol.PLAYER: player, StatsCol.HERO: hero, **season_stats[player][hero]}
             stats_records.append(record)
 
     return pd.DataFrame.from_records(stats_records, columns=[col.value for col in StatsCol])
 
 
 if __name__ == '__main__':
-    ARGS = parse_args().__dict__
-    log_paths = utils.get_log_paths(ARGS[Arg.LOGS_DIR])
+    # ARGS = parse_args().__dict__
+
+    log_paths = utils.get_log_paths(DEFAULT_LOGS_DIR)
 
     season_stats = get_player_season_stats(log_paths)
     season_stats_df = reduce_to_df(season_stats)
     utils.fix_hero_names(season_stats_df)
     season_stats_df = utils.prettify_columns(season_stats_df)
 
-    print(season_stats_df) 
-    season_stats_df.to_csv(ARGS[Arg.OUTPUT], index=False)
-
-
+    print(season_stats_df)
+    season_stats_df.to_csv('season_stats.csv', index=False)
